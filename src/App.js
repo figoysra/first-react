9d6897f6164af045b557b605f8366b42de3b08d7
kokomon <43951215+figoysra@users.noreply.github.com>
2022-10-31 15:35:52 +0700

    a

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          'TESTING'
+          AAAAA
         </p>
       </header>
     </div>
